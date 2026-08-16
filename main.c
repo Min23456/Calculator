98deb9810a12dbd3bcdbb1063aac25ee198c1332
Min23456 <oksumilee@gmail.com>
2026-08-16 17:24:53 -0400

    Added it so you can do: multiple numbers in one equation. (e.g. 1 + 1 + 1, and it only works with subtraction & addition.)

--- main.c
+++ main.c
@@ -25,33 +25,52 @@ double sub(double a, double b){
 
 
 int main(){
-    double a, b;
+
     char op;
     char stop;
     double result;
-    
+    double num;
 
     printf("Calculator in C.\n");
     do {
 
     printf("Please enter a Calculation. (2 numbers + Vaild Operator), (e.g. 2 + 7)\n");
-    scanf(" %lf %c %lf", &a, &op, &b);
+    // scanf(" %lf %c %lf", &a, &op, &b);
+
+    scanf("%lf", &result);
+
+    while (scanf("%c", &op) == 1) {
+
+    if(op == '\n'){
+        break;
+    }
 
     if(op == '+'){
-        result = add(a, b);
+        scanf("%lf", &num);
+        result = add(result, num);
     } else if (op == '-'){
-        result = sub(a, b);
+        scanf("%lf", &num);
+        result = sub(result, num);
     } else if (op == '*'){
-        result = muti(a, b);
+        scanf("%lf", &num);
+        result = muti(result, num);
     } else if (op == '/'){
-        result = divide(a, b);
+        scanf("%lf", &num);
+        result = divide(result, num);
     } else {
         printf("Wrong Operator, please try again!");
     }
     
-    printf("%.0lf %c %.0lf = %.0lf \n", a, op, b, result);
-    printf("Would you like to continue? (If you would like to stop, press q, if you would like to move on, please press on any key other then q.)");
+
+    }
+    printf("%.2lf", result);
+    
+
+    printf("\nWould you like to continue? (If you would like to stop, press q, if you would like to move on, please press on any key other then q.)");
     scanf(" %c" , &stop );
+
+
+
     } while (stop != 'q'); 
 
 

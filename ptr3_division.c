f6bc6d2874bcb3d50425446bb046ff1df6dc483b
Min23456 <oksumilee@gmail.com>
2026-09-07 17:07:04 -0400

    Pointers & in functions.

--- ptr3_division.c
+++ ptr3_division.c
@@ -8,7 +8,14 @@ double division_old(double a, double b){
     return a / b;
 }
 
+int divide_new(double a, double b, double *out) {
+    if (b == 0) {
+        return 0;
+    }
 
+    *out = a / b;
+    return 1;
+}
 
 
 int main(){
@@ -16,8 +23,11 @@ int main(){
     double total;
     double result;
     total = 8;
-    total = divide_old(total, 0);
+    total = division_old(total, 0);
     total = total + 5;
     printf("[Previous] 8/0+5 = %.2lf   <- It's incorrect, but it pretends to be correct.\n\n", total);
+
+    divide_new(total, 0, &result)
+
     return 0;
 }
